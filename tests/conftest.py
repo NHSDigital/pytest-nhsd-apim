96c95e3d6494287d552d45c28712e6dd36f2ab3a
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2025-08-08 11:07:54 +0100

    Fix test run

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,6 @@
-pytest_plugins = 'pytester'
+pytest_plugins = "pytester"
+
+
+def pytest_addoption(parser):
+    parser.addoption("--proxy-name", action="store", help="Name of the proxy")
+    parser.addoption("--api-name", action="store", help="Name of the API")
