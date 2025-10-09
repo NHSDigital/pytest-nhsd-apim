33c070ef615fbbbfed70454238e44ebd80c4c609
Sathiyakumar Ganesan <sathiyakumar.ganesan1@nhs.net>
2025-10-09 15:23:43 +0100

    Updated the token_cache python script

--- src/pytest_nhsd_apim/token_cache.py
+++ src/pytest_nhsd_apim/token_cache.py
@@ -20,7 +20,11 @@ class _TokenCache:
             # only present on app-restricted tokens.  we can inject
             # this ourselves. Assume 5 seconds ago, probably was more
             # recently
-            token_data["issued_at"] = time() - 5000
+            token_data["issued_at"] = (int(time()) * 1000) - 5000
+
+        if not self.is_time_in_milliseconds(token_data["issued_at"]):
+            token_data["issued_at"] = int(token_data["issued_at"]) * 1000
+        
         self._cache[key] = token_data
 
     @log_method
@@ -33,18 +37,26 @@ class _TokenCache:
 
         old_token_data = self._cache[key]
         grace_period_seconds = 5
-        now_ish = int(time()) + grace_period_seconds
+        now_ish = (int(time()) + grace_period_seconds) * 1000
 
         # issued_at is epoch_time in milliseconds
         # but expires_in is in seconds
         # => need factor of 1000 in this sum.
-        expiry_time = int(old_token_data["issued_at"]) + 1000 * int(
-            old_token_data["expires_in"]
-        )
+        if not self.is_expiry_in_milliseconds(old_token_data["expires_in"]):
+            old_token_data["expires_in"] = int(old_token_data["expires_in"]) * 1000
+
+        expiry_time = int(old_token_data["issued_at"]) + int(old_token_data["expires_in"])
         if now_ish > expiry_time:
             self._cache.pop(key)
             return None
         return old_token_data
+    
+    def is_time_in_milliseconds(self, time_value):
+        return time_value > 10**11 #Current time in secs not go over 11 digits
+    
+    def is_expiry_in_milliseconds(self, expiry_value):
+        return expiry_value > 86400 #Assuming seconds not go higher than 24hrs
+        
 
 
 _CACHES = {}
